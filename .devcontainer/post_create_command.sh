dc385cc7bfb92a1d862b593826d0434985cfdbb8
angrybat <jasonmoore95@hotmail.com>
2025-09-06 19:15:52 +0000

    Added command to install production dependencies as part of the post create command

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -5,3 +5,4 @@ mkdir -p /home/vscode/.local/share/bash-completion/completions
 dagger completion bash > /home/vscode/.local/share/bash-completion/completions/dagger
 pip install --upgrade pip==25.2
 pip install -r requirements/development.txt
+pip install -r requirements/production.txt
