399da2df0968dcd719f7e39dd09da0fd01dd8703
angrybat <jasonmoore95@hotmail.com>
2025-09-06 19:01:03 +0000

    Added a pip command to install the dependencies in the development.txt file as a post create command

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -4,3 +4,4 @@ curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.18.17 BIN_D
 mkdir -p /home/vscode/.local/share/bash-completion/completions
 dagger completion bash > /home/vscode/.local/share/bash-completion/completions/dagger
 pip install --upgrade pip==25.2
+pip install -r requirements/development.txt
