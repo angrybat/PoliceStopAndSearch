ab28eb053de03a85068a19c45aec2df47a887a72
angrybat <jasonmoore95@hotmail.com>
2025-09-06 20:17:10 +0000

    Added the available date force mapping model

---
+++ src/models/bronze/available_date_force_mapping.py
@@ -0,0 +1,11 @@
+from sqlmodel import Column, Field, ForeignKey, SQLModel
+
+
+class AvailableDateForceMapping(SQLModel, table=True):
+    __tablename__ = "AvailableDateForceMapping"
+    __table_args__ = {"schema": "bronze"}
+
+    available_date_id = Field(sa_column=Column("AvailableDateId", ForeignKey(
+        "bronze.AvailableDate.Id"), nullable=False, primary_key=True))
+    force_id = Field(sa_column=Column("ForceId", ForeignKey(
+        "bronze.Force.Id"), nullable=False, primary_key=True))
