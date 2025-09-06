fdb14925146f811f59bb315885dddcea99f287fe
angrybat <jasonmoore95@hotmail.com>
2025-09-06 18:57:33 +0000

    added pip command to upgrade pip to version 25.2

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -3,3 +3,4 @@
 curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.18.17 BIN_DIR=$HOME/.local/bin sh
 mkdir -p /home/vscode/.local/share/bash-completion/completions
 dagger completion bash > /home/vscode/.local/share/bash-completion/completions/dagger
+pip install --upgrade pip==25.2
