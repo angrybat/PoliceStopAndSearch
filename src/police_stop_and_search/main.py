28c14f2a18b6427a661c8f55ffc14fd290b0c694
angrybat <jasonmoore95@hotmail.com>
2025-09-06 21:06:29 +0000

    Added two dagger functions one to create a postgres container and another to run this as a service

--- src/police_stop_and_search/main.py
+++ src/police_stop_and_search/main.py
@@ -1,22 +1,43 @@
-import dagger
-from dagger import dag, function, object_type
+from typing import Annotated
+
+from dagger import Container, Doc, dag, function, object_type
+
+TAG_DOCS = "Tag of the postgres image, defaults to 17.6-bookworm"
+USERNAME_DOCS = "Username of the superuser, defaults to postgres"
+PASSWORD_DOCS = "Password of the superuser, defaults to password"
+DATABASE_NAME_DOCS = "Name of the database created, defaults to postgres"
 
 
 @object_type
 class PoliceStopAndSearch:
     @function
-    def container_echo(self, string_arg: str) -> dagger.Container:
-        """Returns a container that echoes whatever string argument is provided"""
-        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
+    def postgres(
+        self,
+        tag: Annotated[str, Doc(TAG_DOCS)] = "17.6-bookworm",
+        username: Annotated[str, Doc(USERNAME_DOCS)] = "postgres",
+        password: Annotated[str, Doc(PASSWORD_DOCS)] = "password",
+        database_name: Annotated[str, Doc(DATABASE_NAME_DOCS)] = "postgres"
+    ) -> Container:
+        """Returns a postgres container"""
+        return (
+            dag.container()
+            .from_(f"postgres:{tag}")
+            .with_env_variable("POSTGRES_USER", username)
+            .with_env_variable("POSTGRES_PASSWORD", password)
+            .with_env_variable("POSTGRES_DB", database_name)
+            .with_exposed_port(5432)
+        )
 
     @function
-    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
-        return await (
-            dag.container()
-            .from_("alpine:latest")
-            .with_mounted_directory("/mnt", directory_arg)
-            .with_workdir("/mnt")
-            .with_exec(["grep", "-R", pattern, "."])
-            .stdout()
+    def postgres_service(
+        self,
+        tag: Annotated[str, Doc(TAG_DOCS)] = "17.6-bookworm",
+        username: Annotated[str, Doc(USERNAME_DOCS)] = "postgres",
+        password: Annotated[str, Doc(PASSWORD_DOCS)] = "password",
+        database_name: Annotated[str, Doc(DATABASE_NAME_DOCS)] = "postgres"
+    ) -> Container:
+        """Returns the postgres container as a service so it can be run via dagger"""
+        return (
+            self.postgres(tag, username, password, database_name)
+            .as_service()
         )
