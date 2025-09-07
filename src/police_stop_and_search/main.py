12be4a316c6f916929f2264b91c6850cc6d4421c
angrybat <jasonmoore95@hotmail.com>
2025-09-07 12:36:58 +0000

    Added the ability to provide a cache to the postgresql database container

--- src/police_stop_and_search/main.py
+++ src/police_stop_and_search/main.py
@@ -8,6 +8,7 @@ USERNAME_DOC = Doc("Username of the superuser")
 PASSWORD_DOC = Doc("Password of the superuser")
 DATABASE_NAME_DOC = Doc("Name of the database created")
 SOURCE_DOC = Doc("Source directory containing the codebase")
+CACHE_VOLUME_DOC = Doc("Name of the cache volume to use")
 USER = "sgt_angle"
 USER_HOME = f"/home/{USER}"
 PIP_CACHE_PATH = f"{USER_HOME}/.cache/pip"
@@ -22,16 +23,25 @@ class PoliceStopAndSearch:
         username: Annotated[str, USERNAME_DOC] = "postgres",
         password: Annotated[str, PASSWORD_DOC] = "password",
         database_name: Annotated[str, DATABASE_NAME_DOC] = "postgres",
+        cache_volume_name: Annotated[str | None, CACHE_VOLUME_DOC] = None,
     ) -> Container:
-        """Returns a postgres container"""
-        return (
+        """Returns a postgres container
+        The data can be cached by providing a cache_volume_name
+        """
+        container = (
             dag.container()
             .from_(f"postgres:{tag}")
             .with_env_variable("POSTGRES_USER", username)
             .with_env_variable("POSTGRES_PASSWORD", password)
             .with_env_variable("POSTGRES_DB", database_name)
+            .with_env_variable("PGDATA", "/var/lib/postgresql/data")
             .with_exposed_port(5432)
         )
+        if cache_volume_name is not None:
+            return container.with_mounted_cache(
+                "/var/lib/postgresql/data", dag.cache_volume(cache_volume_name)
+            )
+        return container
 
     @function
     def postgres_service(
@@ -40,9 +50,12 @@ class PoliceStopAndSearch:
         username: Annotated[str, USERNAME_DOC] = "postgres",
         password: Annotated[str, PASSWORD_DOC] = "password",
         database_name: Annotated[str, DATABASE_NAME_DOC] = "postgres",
+        cache_volume_name: Annotated[str | None, CACHE_VOLUME_DOC] = None,
     ) -> Service:
         """Returns the postgres container as a service so it can be run via dagger"""
-        return self.postgres(tag, username, password, database_name).as_service()
+        return self.postgres(
+            tag, username, password, database_name, cache_volume_name
+        ).as_service(use_entrypoint=True)
 
     @function
     async def production_dependencies(
