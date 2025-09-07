e1b341c9d4e70f45783a35491962dc715b2c3183
angrybat <jasonmoore95@hotmail.com>
2025-09-07 18:12:59 +0000

    Added integration test to check that the available dates is returned

--- tests/test_integration/test_police_api/test_get_data.py
+++ tests/test_integration/test_police_api/test_get_data.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pytest
 
 from src.ingest.police_client import PoliceClient
+from src.models.bronze.available_date import AvailableDate
 from src.models.bronze.force import Force
 
 
@@ -20,6 +21,17 @@ def expected_forces(test_data_directory: Path) -> list[Force]:
     return [Force(**force) for force in expected_forces]
 
 
+@pytest.fixture
+def expected_available_dates(test_data_directory: Path) -> list[AvailableDate]:
+    expected_available_dates_path = test_data_directory / "available_dates.json"
+    with expected_available_dates_path.open("r") as file:
+        expected_available_dates = json.load(file)
+    return [
+        AvailableDate(id=None, year_month=available_date["date"])
+        for available_date in expected_available_dates
+    ]
+
+
 class TestGetForces:
     @pytest.mark.asyncio
     async def test_get_forces(self, expected_forces: list[Force]):
@@ -28,3 +40,16 @@ class TestGetForces:
         forces = await police_client.get_forces()
 
         assert forces == expected_forces
+
+
+class TestGetAvailableDates:
+    @pytest.mark.asyncio
+    async def test_get_available_dates(
+        self, expected_available_dates: list[AvailableDate]
+    ):
+        police_client = PoliceClient()
+
+        available_dates = await police_client.get_available_dates()
+
+        for expected_date in expected_available_dates:
+            assert expected_date in available_dates
