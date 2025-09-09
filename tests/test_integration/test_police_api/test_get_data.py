b3999992a53f9f1af1db7d9cf0d0ea9ba6a37f01
angrybat <jasonmoore95@hotmail.com>
2025-09-09 17:34:51 +0000

    Moved test data to the route of integration tests and moved logic for reading it to conftest

--- tests/test_integration/test_police_api/test_get_data.py
+++ tests/test_integration/test_police_api/test_get_data.py
@@ -1,7 +1,3 @@
-import json
-from pathlib import Path
-from typing import Any
-
 import pytest
 
 from src.ingest.police_client import (
@@ -10,59 +6,6 @@ from src.ingest.police_client import (
 from src.models.bronze.available_date import AvailableDate
 from src.models.bronze.force import Force
 from src.models.bronze.stop_and_search import StopAndSearch
-
-
-@pytest.fixture
-def test_data_directory() -> Path:
-    return Path("tests/test_integration/test_police_api/test_data")
-
-
-@pytest.fixture
-def expected_forces(test_data_directory: Path) -> list[Force]:
-    return [
-        Force.model_validate(force)
-        for force in get_json_from_file(test_data_directory, "forces.json")
-    ]
-
-
-@pytest.fixture
-def expected_available_dates(test_data_directory: Path) -> list[AvailableDate]:
-    return [
-        AvailableDate.model_validate(force)
-        for force in get_json_from_file(test_data_directory, "available_dates.json")
-    ]
-
-
-@pytest.fixture
-def expected_stop_and_searches_with_location(
-    test_data_directory: Path,
-) -> list[StopAndSearch]:
-    return [
-        StopAndSearch.model_validate(stop_and_search)
-        for stop_and_search in get_json_from_file(
-            test_data_directory, "stop_and_searches_with_location.json"
-        )
-    ]
-
-
-@pytest.fixture
-def expected_stop_and_searches_without_location(
-    test_data_directory: Path,
-) -> list[StopAndSearch]:
-    return [
-        StopAndSearch.model_validate(stop_and_search)
-        for stop_and_search in get_json_from_file(
-            test_data_directory, "stop_and_searches_without_location.json"
-        )
-    ]
-
-
-def get_json_from_file(
-    test_data_directory: Path, file_name: str
-) -> list[dict[str, Any]]:
-    expected_data_path = test_data_directory / file_name
-    with expected_data_path.open("r") as file:
-        return json.load(file)
 
 
 class TestGetForces:
