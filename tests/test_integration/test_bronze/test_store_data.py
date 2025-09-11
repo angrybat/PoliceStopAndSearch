ae5317729b51178903269813aadd869fa4509029
angrybat <jasonmoore95@hotmail.com>
2025-09-11 18:34:40 +0000

    Added integration test to check available dates are stored

--- tests/test_integration/test_bronze/test_store_data.py
+++ tests/test_integration/test_bronze/test_store_data.py
@@ -3,10 +3,12 @@ from collections.abc import Generator
 
 import pytest
 from sqlalchemy import Engine, create_engine
+from sqlalchemy.orm import joinedload
 from sqlmodel import Session, SQLModel, inspect, select, text
 
-from src.ingest.force_repository import ForceRepository
+from src.ingest.available_date_repository import AvailableDateRepository
 from src.ingest.police_client import PoliceClient
+from src.models.bronze.available_date import AvailableDate
 from src.models.bronze.force import Force
 
 DEFAULT_DB_URL = "postgresql+psycopg2://postgres:password@localhost:5432/postgres"
@@ -30,38 +32,46 @@ def police_client() -> PoliceClient:
 
 
 @pytest.fixture()
-def force_repository(
+def available_date_repository(
     engine: Engine, police_client: PoliceClient
-) -> Generator[ForceRepository, None, None]:
+) -> Generator[AvailableDateRepository, None, None]:
+    setup_database(engine)
+    yield AvailableDateRepository(engine, police_client)
+
+
+def setup_database(engine):
     with engine.connect() as connection:
         inspector = inspect(connection)
         schemas = inspector.get_schema_names()
-        if bronze_not_exists := "bronze" not in schemas:
+        SQLModel.metadata.drop_all(connection)
+        if "bronze" not in schemas:
             connection.execute(text("CREATE SCHEMA BRONZE"))
-        if table_exists := inspector.has_table(Force.__tablename__, "bronze"):
-            SQLModel.metadata.drop_all(connection)
         SQLModel.metadata.create_all(connection)
         connection.commit()
-        yield ForceRepository(engine, police_client)
-        SQLModel.metadata.drop_all(connection)
-        if table_exists:
-            SQLModel.metadata.create_all(connection)
-        if bronze_not_exists:
-            connection.execute(text("DROP SCHEMA BRONZE"))
-        connection.commit()
 
 
-class TestForceRepository:
+class TestAvailableDateRepository:
     @pytest.mark.asyncio
-    async def test_store_forces(
+    async def test_store_available_dates(
         self,
         engine: Engine,
-        force_repository: ForceRepository,
+        available_date_repository: AvailableDateRepository,
+        expected_available_dates: list[AvailableDate],
         expected_forces: list[Force],
     ) -> None:
-        forces = await force_repository.store_forces()
+        await available_date_repository.store_available_dates()
 
-        assert forces == expected_forces
         with Session(engine) as session:
             stored_forces = session.exec(select(Force)).all()
-        assert stored_forces == expected_forces
+            stored_available_dates = (
+                session.exec(
+                    select(AvailableDate).options(joinedload(AvailableDate.forces))  # type: ignore
+                )
+                .unique()
+                .all()
+            )
+        assert sorted(stored_forces, key=lambda force: force.id) == sorted(
+            expected_forces, key=lambda force: force.id
+        )
+        for expected_date in expected_available_dates:
+            assert expected_date in stored_available_dates
