b9963c65cadea649cb3a4f1b806f4f154ac87b4d
angrybat <jasonmoore95@hotmail.com>
2025-09-12 21:36:15 +0000

    Fixed failing unit tests

--- src/ingest/repositories/force_repository.py
+++ src/ingest/repositories/force_repository.py
@@ -1,4 +1,8 @@
+from logging import Logger, getLogger
+
+from httpx import HTTPStatusError
 from sqlalchemy import Engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlmodel import Session
 
 from src.ingest.police_client import PoliceClient
@@ -6,15 +10,31 @@ from src.models.bronze.force import Force
 
 
 class ForceRepository:
-    def __init__(self, engine: Engine, police_client: PoliceClient):
+    def __init__(
+        self, engine: Engine, police_client: PoliceClient, logger: Logger | None = None
+    ):
         self.engine = engine
         self.police_client = police_client
+        self.logger = logger or getLogger("ForceRepository")
 
-    async def store_forces(self) -> list[Force]:
-        forces = await self.police_client.get_forces()
+    async def store_forces(self) -> list[Force] | None:
+        try:
+            forces = await self.police_client.get_forces()
+        except HTTPStatusError:
+            return None
+
         with Session(self.engine) as session:
-            session.add_all(forces)
-            session.commit()
-            for force in forces:
-                session.refresh(force)
+            try:
+                session.add_all(forces)
+                session.commit()
+            except SQLAlchemyError:
+                self.logger.exception("Could not store Forces in the database.")
+                return None
+
+            try:
+                for force in forces:
+                    session.refresh(force)
+            except SQLAlchemyError:
+                self.logger.exception("Could not refresh Forces in the database.")
+                return None
         return forces
