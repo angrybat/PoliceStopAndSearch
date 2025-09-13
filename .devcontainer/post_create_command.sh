8f368124d7f8faf6e6301867302f08e27efa54fa
angrybat <jasonmoore95@hotmail.com>
2025-09-13 19:19:34 +0000

    added pip with a required verison to the pyproject.toml

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -3,7 +3,6 @@
 curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.18.17 BIN_DIR=$HOME/.local/bin sh
 mkdir -p /home/vscode/.local/share/bash-completion/completions
 dagger completion bash > /home/vscode/.local/share/bash-completion/completions/dagger
-pip install --upgrade pip==25.2
 pip install .[dev]
 pip install -e .
 export DATABASE_URL="postgresql+psycopg2://postgres:password@localhost/postgres"
