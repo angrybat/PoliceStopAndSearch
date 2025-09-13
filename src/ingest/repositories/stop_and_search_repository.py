97f5126eaa358b9a5dbc0dfa163dbd27d49f1531
angrybat <jasonmoore95@hotmail.com>
2025-09-13 12:33:52 +0000

    Refactored stop_and_search_repository to use the available date repository

--- src/ingest/repositories/stop_and_search_repository.py
+++ src/ingest/repositories/stop_and_search_repository.py
@@ -5,17 +5,17 @@ from logging import Logger, getLogger
 from httpx import HTTPStatusError
 from sqlalchemy import Engine
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import joinedload
-from sqlmodel import Session, and_, select
+from sqlmodel import Session
 
 from src.ingest.police_client import PoliceClient
-from src.models.bronze.available_date import AvailableDate
+from src.ingest.repositories.available_date_repository import AvailableDateRepository
 
 
 class StopAndSearchRepository:
     def __init__(
         self, engine: Engine, police_client: PoliceClient, logger: Logger | None = None
     ):
+        self.available_date_repository = AvailableDateRepository(engine, police_client)
         self.engine = engine
         self.police_client = police_client
         self.logger = logger or getLogger("StopAndSearchRepository")
@@ -23,40 +23,22 @@ class StopAndSearchRepository:
     async def store_stop_and_searches(
         self, from_datetime: datetime, to_datetime: datetime
     ) -> bool:
-        with Session(self.engine) as session:
-            try:
-                available_dates = (
-                    session.exec(
-                        select(AvailableDate)
-                        .where(
-                            and_(
-                                from_datetime.strftime("%Y-%m")
-                                <= AvailableDate.year_month,
-                                AvailableDate.year_month
-                                <= to_datetime.strftime("%Y-%m"),
-                            )
-                        )
-                        .options(joinedload(AvailableDate.forces))  # type: ignore
-                    )
-                    .unique()
-                    .all()
-                )
-            except SQLAlchemyError:
-                self.logger.exception(
-                    "Cannot retreive AvailableDates from the database."
-                )
-                return False
-
-        results = await gather(
-            *[
-                self.store_stop_and_search(
-                    available_date.year_month, force.id, from_datetime, to_datetime
-                )
-                for available_date in available_dates
-                for force in available_date.forces
-            ]
+        available_dates = await self.available_date_repository.get_available_dates(
+            from_datetime, to_datetime, with_forces=True
         )
-        return all(results)
+
+        if available_dates:
+            results = await gather(
+                *[
+                    self.store_stop_and_search(
+                        available_date.year_month, force.id, from_datetime, to_datetime
+                    )
+                    for available_date in available_dates
+                    for force in available_date.forces
+                ]
+            )
+            return all(results)
+        return False
 
     async def store_stop_and_search(
         self, date: str, force_id: str, from_datetime: datetime, to_datetime: datetime
