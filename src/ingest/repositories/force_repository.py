a85e7df44d8eb93517abae6ba035932afcb161fb
angrybat <jasonmoore95@hotmail.com>
2025-09-13 00:20:08 +0000

    Fixed failing unit test

--- src/ingest/repositories/force_repository.py
+++ src/ingest/repositories/force_repository.py
@@ -1,9 +1,10 @@
+from asyncio import gather
 from logging import Logger, getLogger
 
 from httpx import HTTPStatusError
 from sqlalchemy import Engine
 from sqlalchemy.exc import SQLAlchemyError
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from src.ingest.police_client import PoliceClient
 from src.models.bronze.force import Force
@@ -19,22 +20,36 @@ class ForceRepository:
 
     async def store_forces(self) -> list[Force] | None:
         try:
-            forces = await self.police_client.get_forces()
+            forces, existing_forces = await gather(
+                self.police_client.get_forces(), self.get_all_forces()
+            )
         except HTTPStatusError:
             return None
 
+        if existing_forces is None:
+            return None
+
         with Session(self.engine) as session:
+            forces_to_add = [force for force in forces if force not in existing_forces]
             try:
-                session.add_all(forces)
+                session.add_all(forces_to_add)
                 session.commit()
             except SQLAlchemyError:
                 self.logger.exception("Could not store Forces in the database.")
                 return None
 
             try:
-                for force in forces:
+                for force in forces_to_add:
                     session.refresh(force)
             except SQLAlchemyError:
                 self.logger.exception("Could not refresh Forces in the database.")
                 return None
-        return forces
+        return existing_forces + forces_to_add
+
+    async def get_all_forces(self) -> list[Force] | None:
+        try:
+            with Session(self.engine) as session:
+                return list(session.exec(select(Force)).all())
+        except SQLAlchemyError:
+            self.logger.exception("Cannot get existing Forces from the database.")
+            return None
