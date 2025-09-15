a1d0ec9e689a66d1726e8cbb8ba03ddd99ce92ef
angrybat <jasonmoore95@hotmail.com>
2025-09-15 16:51:30 +0000

    updated post install script to isntall development dependenicies and the police api packge in one command

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -3,6 +3,5 @@
 curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.18.17 BIN_DIR=$HOME/.local/bin sh
 mkdir -p /home/vscode/.local/share/bash-completion/completions
 dagger completion bash > /home/vscode/.local/share/bash-completion/completions/dagger
-pip install .[dev]
-pip install -e .
+pip install -e .[dev]
 export DATABASE_URL="postgresql+psycopg2://postgres:password@localhost/postgres"
